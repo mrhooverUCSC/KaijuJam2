ba92e6c625303de81ea521f0451140870034de36
matthooverslac <61717155+mrhooverUCSC@users.noreply.github.com>
2023-03-27 21:13:01 -0700

    Concept Art

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public static TitleManager Instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region menu functions
+    public void EnterLevel()
+    {
+        Debug.Log("entering combat");
+        SceneManager.LoadScene("Combat Scene");
+    }
+    #endregion
+}
