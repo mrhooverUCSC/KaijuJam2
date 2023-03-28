3fe6fbc62dbc41c8434485242619c9db39ed4561
matthooverslac <61717155+mrhooverUCSC@users.noreply.github.com>
2023-03-27 23:05:11 -0700

    Cannon Fire Basics
    Cannon squads sound off in the order they will be ready: call [press] them in order. Little circles show if the correct order or not
    i could imagine little pixel art tanks showing up in the correct order that shoot when the number is clicked
    i can also add a timer, or something like that
    damage probably based on number correct

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    #region cannon variables
+    [SerializeField] List<int> CannonFire;
+    int currentCannon;
+    int currentCannonFails;
+    [SerializeField] GameObject cannonUI;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #region FireCannons
+    //4 cannons fire, in a random order. Press them in the correct order
+    public void FireCannons()
+    {
+        StartCoroutine(fire());
+    }
+    public IEnumerator fire()
+    {
+        //shuffle the inputs
+        Shuffle(CannonFire);
+        currentCannon = 0;
+        //turn them on in order
+        yield return new WaitForSeconds(.5f);
+        cannonUI.transform.GetChild(CannonFire[0]).gameObject.SetActive(true);
+        yield return new WaitForSeconds(.4f);
+        cannonUI.transform.GetChild(CannonFire[1]).gameObject.SetActive(true);
+        yield return new WaitForSeconds(.4f);
+        cannonUI.transform.GetChild(CannonFire[2]).gameObject.SetActive(true);
+        yield return new WaitForSeconds(.4f);
+        cannonUI.transform.GetChild(CannonFire[3]).gameObject.SetActive(true);
+        yield return new WaitForSeconds(.4f);
+        cannonUI.transform.GetChild(CannonFire[4]).gameObject.SetActive(true);
+        //signal start
+        yield return new WaitForSeconds(1f);
+        cannonUI.transform.GetChild(5).gameObject.SetActive(true);
+        //enable buttons
+        for(int i = 0; i < 5; ++i)
+        {
+            cannonUI.transform.GetChild(i).GetComponent<Button>().interactable = true;
+        }
+    }
+    public void FireCannonIndividual(int cannonNumber)
+    {
+        if(CannonFire[currentCannon] == cannonNumber)
+        {
+            cannonUI.transform.GetChild(6 + currentCannon).GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            cannonUI.transform.GetChild(6 + currentCannon).GetComponent<Image>().color = Color.red;
+            currentCannonFails++;
+        }
+        cannonUI.transform.GetChild(6 + currentCannon).gameObject.SetActive(true);
+        currentCannon++;
+        if(currentCannon == 5)
+        {
+            CheckCannons();
+        }
+    }
+    void CheckCannons()
+    {
+        cannonUI.transform.GetChild(5).GetComponent<Image>().color = Color.green;
+    }
+    #endregion
+    //list shuffler
+    static void Shuffle<T>(List<T> ts)
+    {
+        var count = ts.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = ts[i];
+            ts[i] = ts[r];
+            ts[r] = tmp;
+        }
+    }
+
+}
