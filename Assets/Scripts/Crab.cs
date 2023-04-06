d0493ec69d45bfe1ab178f6e9a6c493621d3e5a7
Kabenzo <enzoschlatter@gmail.com>
2023-04-05 23:32:26 -0700

    zfasf

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -11,18 +11,30 @@ public class Crab : MonoBehaviour
     [SerializeField] TextMeshProUGUI distanceText;
     [SerializeField] TextMeshProUGUI pitfallText;
     [SerializeField] GameManager gm;
+    [SerializeField] GameObject startPosition;
+    [SerializeField] GameObject endPosition;
+    Vector3 targetPosition;
+    bool crabTurn = false;
     public int pitfalls = 0;
     // Start is called before the first frame update
     void Start()
     {
-        distanceText.text = distance + " meters until CRABOCALYPSE!!";
+        distanceText.text = distance + " meters until CRABOCALYPSE";
         healthText.text = "Crab Health: " + health.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (crabTurn && targetPosition != null && Vector3.Distance(targetPosition, transform.position) > .5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, .1f);
+        }
+        else if (crabTurn && targetPosition != null && Vector3.Distance(targetPosition, transform.position) <= .5)
+        {
+            crabTurn = false;
+            gm.ReadyPlayerTurn();
+        }
     }
 
     public void AttachToCrab(GameManager g)
@@ -43,12 +55,18 @@ public class Crab : MonoBehaviour
     public void Move(int dis)
     {
         distance -= dis;
-        distanceText.text = distance + " meters until CRABOCALYPSE!!";
-        if(distance <= 0)
+        distanceText.text = distance + " meters until CRABOCALYPSE";
+        Debug.Log(distance);
+        float percentThere = (1.0f - distance / 1000.0f);
+        print(percentThere);
+        targetPosition = startPosition.transform.position + new Vector3((endPosition.transform.position.x - startPosition.transform.position.x) * percentThere, (endPosition.transform.position.y - startPosition.transform.position.y) * percentThere, (endPosition.transform.position.z - startPosition.transform.position.z) * percentThere); //startPosition Vector3.Distance(startPosition.transform.position, endPosition.transform.position) / (1 - dis/1000);
+        Debug.Log(targetPosition);
+        if (distance <= 0)
         {
             Debug.Log("Game Lost");
             gm.Failure();
         }
+        crabTurn = true;
     }
 
     public void AddPitfall()
@@ -59,16 +77,15 @@ public class Crab : MonoBehaviour
 
     public void EnemyTurn()
     {
-        int temp = (int)Random.Range(0, 2);
-        Debug.Log(temp);
-        if(temp == 0)
+        int temp = 0;// (int)Random.Range(0, 2);
+        if (temp == 0)
         {
-            if(pitfalls > 0)
+            if (pitfalls > 0)
             {
                 TakeDamage(2);
                 Move(15);
                 pitfalls--;
-                if(pitfalls > 0)
+                if (pitfalls > 0)
                 {
                     pitfallText.text = pitfalls + "Pitfalls";
                 }
@@ -85,8 +102,8 @@ public class Crab : MonoBehaviour
         else
         {
             gm.DamagePlayer(5);
+            gm.ReadyPlayerTurn();
         }
-        gm.ReadyPlayerTurn();
     }
 
 
